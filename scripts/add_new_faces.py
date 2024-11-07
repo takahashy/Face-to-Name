ecfcf5ea57ee3dc0c575d1f52d7e177229922a32
T Y <takahashy@Ts-MacBook-Air.lan>
2024-11-07 11:34:43 -0500

    modularized add new faces script

--- scripts/add_new_faces.py
+++ scripts/add_new_faces.py
@@ -10,55 +10,49 @@ from modules.db_manager import DBManager
 from modules.utils import isDirectory, imageExists, processImage
 
 DATA_DIR = Path(__file__).parent.parent / "data"
+DB_MANAGER = DBManager()
+
+def iterateDir(dir: Path, name:str) -> None:
+    """
+    iterate through the images in the name directory and add them to database
+    """
+    image_paths, embeddings = [], []
+    for image in dir.iterdir():
+        if imageExists(image):
+            embedding = processImage(str(image))[1]
+
+            if embedding:
+                image_paths.append(str(image))
+                embeddings += embedding
+            else:
+                print(f"IMAGE WARNING: Cannot find facial features in {image}")
+        
+        print(f"Processing {image.name}...")
+    DB_MANAGER.insertFaces(name, image_paths, embeddings)
+    print()
 
 def main(arg):
     """
-    should read a name directory or all directories
+    reads from a name directory or all directories in the data directory
+    adds new faces to the database.
     """
-    db_manager = DBManager()
-    image_paths, embeddings = [], []
-
     if arg == "all":
         for subdir in DATA_DIR.iterdir():
-            image_paths, embeddings = [], []
             name = subdir.name
             if subdir.is_dir() and name != "test":
                 print(f"----Reading {name}----")
-                for image in subdir.iterdir():
-                    if imageExists(image):
-                        embedding = processImage(str(image))[1]
-
-                        if embedding:
-                            image_paths.append(str(image))
-                            embeddings += embedding
-                        else:
-                            print(f"IMAGE WARNING: Cannot find facial features in {image}")
-                    
-                    print(f"Processing {image.name}...")
-                db_manager.insertFaces(name, image_paths, embeddings)
-                print()
+                iterateDir(subdir, name)
 
     elif isDirectory("data/" + arg):
-        for image in (DATA_DIR / arg).iterdir():
-            if imageExists(image):
-                embedding = processImage(str(image))[1]
-
-                if embedding:
-                    image_paths.append(str(image))
-                    embeddings += embedding
-                else:
-                    print(f"IMAGE WARNING: Cannot find facial features in {image}")
-            
-            print(f"Processing {image.name}...")
-        db_manager.insertFaces(arg, image_paths, embeddings)
+        iterateDir(DATA_DIR / arg, arg)
 
     else:
         print("PATH ERROR: `{image_file}` does not exist") 
         sys.exit(1)
 
-
     print(f"\33[92mDone!\033[0m")
-    db_manager.close()
+    DB_MANAGER.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 :
